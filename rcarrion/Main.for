4a10f655016bd74464f76be7b555ad1b47e579e1
giulianobelinassi <giuliano.belinassi@usp.br>
2016-12-13 20:33:30 -0200

    Compila, executa! :-)

--- rcarrion/Main.for
+++ rcarrion/Main.for
@@ -1,137 +1,143 @@
 ************************************************************************
 ************************************************************************
 **                                                                    **
-**     ESTE PROGRAMA RESOLVE PROBLEMAS VISCOELASTICOS, DINÂMICOS, 	**
+**     ESTE PROGRAMA RESOLVE PROBLEMAS VISCOELASTICOS, DINÂMICOS,   **
 **                                                                    **
-**   TRIDIMENSIONAIS ATRAVÉS DO MÉTODO DOS ELEMENTOS DE CONTORNO NUM	**
-**																	**
-**						SEMI ESPAÇO INFINITO						**
-**																	**
+**   TRIDIMENSIONAIS ATRAVÉS DO MÉTODO DOS ELEMENTOS DE CONTORNO NUM    **
+**                                                                  **
+**                      SEMI ESPAÇO INFINITO                        **
+**                                                                  **
 ************************************************************************
 **                                                                    **
 ** PROGRAMA "MECECCS" = (MÉTODO DOS ELEMENTOS DE CONTORNO USANDO      **
 **                                                                    **
-**					   ELEMENTOS CONSTANTES COM SINGULARIDADE)		**
-**																	**
-**								DINÂMICO							**
-**																	**
+**                     ELEMENTOS CONSTANTES COM SINGULARIDADE)      **
+**                                                                  **
+**                              DINÂMICO                            **
+**                                                                  **
 ** RONALDO CARRION / EUCLIDES DE MESQUITA NETO                        **
 **                                                                    **
-** DATA: 18/07/01														**
-**																	**
+** DATA: 18/07/01                                                       **
+**                                                                  **
 ************************************************************************
 ************************************************************************
 *
-	PROGRAM MAIN
-C		USE MSIMSL
+      PROGRAM MAIN
+C       USE MSIMSL
 *
-		IMPLICIT REAL*8 (A-H,O-Y)
-		IMPLICIT COMPLEX*16 (Z)
-		PARAMETER (NE=960,NX=3*NE,NCOX=962,NPIX=10,NFRX=7)
-		COMMON INP,INQ,IPR,IPS,IPT
-		DIMENSION CX(NCOX),CY(NCOX),CZ(NCOX)
-		DIMENSION CXM(NE),CYM(NE),CZM(NE)
-		DIMENSION CXI(NPIX),CYI(NPIX),CZI(NPIX)
-		DIMENSION HEST(NX,NX),GEST(NX,NX)
-		DIMENSION ZH(NX,NX),ZG(NX,NX)
-		DIMENSION ZDFI(NX),ZFI(NX),ZVETSOL(NX),ZDSOL(3*NPIX),ZSSOL(9*NPIX)
-		DIMENSION DELTA(3,3)
-		DIMENSION BC(NX),DFI(NX),AFR(NFRX)
-		INTEGER CONE(NE,4),KODE(NX)
-		REAL INSTANTE_INICIAL,INSTANTE_FINAL
-C		Declara a matriz de pivoteamento para o LAPACK.
-		INTEGER P(1000)
-		COMPLEX*16 WORK(1000)
-		COMPLEX   SWORK(1000000)
-		DOUBLE PRECISION RWORK(1000)
-		INTEGER ITER
+        IMPLICIT REAL*8 (A-H,O-Y)
+        IMPLICIT COMPLEX*16 (Z)
+        PARAMETER (NE=960,NX=3*NE,NCOX=962,NPIX=10,NFRX=7)
+        COMMON INP,INQ,IPR,IPS,IPT
+        DIMENSION CX(NCOX),CY(NCOX),CZ(NCOX)
+        DIMENSION CXM(NE),CYM(NE),CZM(NE)
+        DIMENSION CXI(NPIX),CYI(NPIX),CZI(NPIX)
+        DIMENSION HEST(NX,NX),GEST(NX,NX)
+        DIMENSION ZH(NX,NX),ZG(NX,NX)
+        DIMENSION ZDFI(NX),ZFI(NX),ZVETSOL(NX),ZDSOL(3*NPIX)
+        DIMENSION ZSSOL(9*NPIX)
+        DIMENSION DELTA(3,3)
+        DIMENSION BC(NX),DFI(NX),AFR(NFRX)
+        INTEGER CONE(NE,4),KODE(NX)
+        REAL INSTANTE_INICIAL,INSTANTE_FINAL
+C       Declara a matriz de pivoteamento para o LAPACK.
+        INTEGER P(1000)
+        COMPLEX*16 WORK(1000)
+        COMPLEX   SWORK(1000000)
+        DOUBLE PRECISION RWORK(1000)
+        INTEGER ITER
 
 *
 * NE = NÚMERO MÁXIMO DE ELEMENTOS DA MALHA (CONTORNO + ENCLOSING)
-* NX = DIMENSÃO MÁXIMA DO SISTEMA DE EQUAÇÕES		
+* NX = DIMENSÃO MÁXIMA DO SISTEMA DE EQUAÇÕES       
 * NCOX = NÚMERO MÁXIMO DE PONTOS EXTREMOS DOS ELEMENTOS (NE+2)
 * NPIX = NÚMERO MÁXIMO DE PONTOS INTERNOS
 *
 * PARÂMETROS ESPECIFICADORES DOS NÚMEROS DE UNIDADE (ARQUIVO) DE E/S
 *
-		INP=5
-		INQ=6
-		IPR=7
-		IPS=8
-		IPT=9
-      
-		OPEN(UNIT=INP,FILE='ESOLO240E_-5+5_e10.DAT',STATUS='OLD')
-      	OPEN(UNIT=INQ,FILE='ESOLO240D_-5+5_e10.DAT',STATUS='OLD')
-      	OPEN(UNIT=IPR,FILE='SSOLO240E_-5+5_e10.DAT',STATUS='UNKNOWN')
-      	OPEN(UNIT=IPS,FILE='SSOLO240D_-5+5_e10.DAT',STATUS='UNKNOWN')
-C     	OPEN(UNIT=IPT,FILE='GSOLO240_a5b5.DAT',STATUS='UNKNOWN')
+        INP=5
+        INQ=6
+        IPR=7
+        IPS=8
+        IPT=9
+
+        OPEN(UNIT=INP,FILE='ESOLO240E_-5+5.DAT',STATUS='OLD')
+        OPEN(UNIT=INQ,FILE='ESOLO240D_-5+5.DAT',STATUS='OLD')
+        OPEN(UNIT=IPR,FILE='SSOLO240E_-5+5.DAT',STATUS='UNKNOWN')
+        OPEN(UNIT=IPS,FILE='SSOLO240D_-5+5.DAT',STATUS='UNKNOWN')
+
+C       OPEN(UNIT=INP,FILE='ESOLO240E_-5+5_e10.DAT',STATUS='OLD')
+C       OPEN(UNIT=INQ,FILE='ESOLO240D_-5+5_e10.DAT',STATUS='OLD')
+C       OPEN(UNIT=IPR,FILE='SSOLO240E_-5+5_e10.DAT',STATUS='UNKNOWN')
+C       OPEN(UNIT=IPS,FILE='SSOLO240D_-5+5_e10.DAT',STATUS='UNKNOWN')
+C       OPEN(UNIT=IPT,FILE='GSOLO240_a5b5.DAT',STATUS='UNKNOWN')
 *
-		CALL CPU_TIME(INSTANTE_INICIAL)
+        CALL CPU_TIME(INSTANTE_INICIAL)
 *
 * ACIONA ROTINA QUE LÊ OS DADOS DE ENTRADA PARA O PROBLEMA ESTÁTICO
 *
       
-      	CALL INPUTECE (CX,CY,CZ,NE,NCOX,CONE,CXM,CYM,CZM,
+        CALL INPUTECE (CX,CY,CZ,NE,NCOX,CONE,CXM,CYM,CZM,
      $N,NBE,NP,NPG,GE,RNU,RMU)
 *
 * ACIONA ROTINA QUE CALCULA AS MATRIZES [H] E [G] DO PROBLEMA ESTÁTICO
 *
-      	CALL GHMATECE (CX,CY,CZ,CXM,CYM,CZM,HEST,GEST,NE,NX,NCOX,CONE,
+        CALL GHMATECE (CX,CY,CZ,CXM,CYM,CZM,HEST,GEST,NE,NX,NCOX,CONE,
      $N,NBE,NP,NPG,GE,RNU,RMU,DELTA,PI,C1,C2,C3,C4)
 *
 * ACIONA ROTINA QUE LÊ OS DADOS DE ENTRADA PARA O PROBLEMA DINÂMICO
 *
-      	CALL INPUTECD (CX,CY,CZ,CXI,CYI,CZI,KODE,BC,NFR,AFR,NE,NX,
+        CALL INPUTECD (CX,CY,CZ,CXI,CYI,CZI,KODE,BC,NFR,AFR,NE,NX,
      $NCOX,NPIX,NFRX,CONE,CXM,CYM,CZM,N,NBE,NP,NPG,GE,RNU,RMU,
      $L,FR,DAM,RHO,ZGE,ZCS,ZCP)
-	
-		NN=3*NBE
-		DO 40 J=1,NN
-			DFI(J)=BC(J)
- 40		CONTINUE
-		DO 12 I=1,NFR
-			FR=AFR(I)
-		WRITE(*,*)'rodando para frequencia ',i
+    
+        NN=3*NBE
+        DO 40 J=1,NN
+            DFI(J)=BC(J)
+ 40     CONTINUE
+        DO 12 I=1,NFR
+            FR=AFR(I)
+        WRITE(*,*)'rodando para frequencia ',i
 *
 * ACIONA ROTINA QUE CALCULA AS MATRIZES [H] E [G] DO PROBLEMA ESTÁTICO
 *
-      	CALL GHMATECD (CX,CY,CZ,CXM,CYM,CZM,HEST,GEST,ZH,ZG,ZFI,DFI,ZDFI,
-     $KODE,NE,NX,NCOX,CONE,DELTA,PI,N,NBE,NP,NPG,GE,RNU,RMU,
-     $L,FR,DAM,RHO,ZGE,ZCS,ZCP,C1,C2,C3,C4)
+        CALL GHMATECD (CX,CY,CZ,CXM,CYM,CZM,HEST,GEST,ZH,ZG,ZFI,DFI,
+     $    ZDFI,KODE,NE,NX,NCOX,CONE,DELTA,PI,N,NBE,NP,NPG,GE,RNU,RMU,
+     $    L,FR,DAM,RHO,ZGE,ZCS,ZCP,C1,C2,C3,C4)
 *
 * ACIONA ROTINA QUE RESOLVE O SISTEMA DE EQUAÇÕES
 *
-* (ROTINA PRONTA DA BIBLIOTECA DO FORTRAN PS 4.0)	
+* (ROTINA PRONTA DA BIBLIOTECA DO FORTRAN PS 4.0)   
 *
-C      	CALL DLSACG(NN,ZH,NX,ZFI,1,ZVETSOL)
-		CALL ZCGESV(NN,1,ZH,NN,P,ZFI,NN,VETSOL,NN,WORK,SWORK,RWORK,ITER)
-		IF (ITER /= 0) THEN
-			PRINT *, "Erro em ZCGESV :-("
-		ENDIF
-		ZFI=ZVETSOL
+C       CALL DLSACG(NN,ZH,NX,ZFI,1,ZVETSOL)
+        CALL ZCGESV(NN,1,ZH,NN,P,ZFI,NN,VETSOL,NN,WORK,SWORK,RWORK,ITER)
+        IF (ITER /= 0) THEN
+            PRINT *, "Erro em ZCGESV :-("
+        ENDIF
+        ZFI=ZVETSOL
 *
 * ACIONA ROTINA QUE CALCULA OS DESLOCAMENTOS EM PONTOS INTERNOS
 *
-		CALL INTEREC(ZFI,ZDFI,KODE,CX,CY,CZ,CXI,CYI,CZI,ZDSOL,ZSSOL,NE,NX,
-     $NCOX,NPIX,CONE,ZGE,ZCS,ZCP,DELTA,PI,FR,NPG,L,N,NBE,RHO)
+        CALL INTEREC(ZFI,ZDFI,KODE,CX,CY,CZ,CXI,CYI,CZI,ZDSOL,ZSSOL,NE,
+     $      NX,NCOX,NPIX,CONE,ZGE,ZCS,ZCP,DELTA,PI,FR,NPG,L,N,NBE,RHO)
 *
 * ACIONA ROTINA QUE IMPRIME AS VARIÁVEIS NO CONTORNO E PONTOS INTERNOS
 *
-     	 CALL OUTPUTEC(ZFI,ZDFI,ZDSOL,ZSSOL,NPIX,NX,N,NBE,L,FR)
- 12	CONTINUE
+         CALL OUTPUTEC(ZFI,ZDFI,ZDSOL,ZSSOL,NPIX,NX,N,NBE,L,FR)
+ 12      CONTINUE
 *
-      	CLOSE (INP)          
-      	CLOSE (INQ)
-      	CLOSE (IPR)          
-      	CLOSE (IPS)
-C       CLOSE (IPT)
-		CALL CPU_TIME(INSTANTE_FINAL)
-		WRITE (*,*)'TEMPO DE PROCESSAMENTO: ',
-     $INSTANTE_FINAL-INSTANTE_INICIAL,' SEGUNDOS'
-		PAUSE
+         CLOSE (INP)          
+         CLOSE (INQ)
+         CLOSE (IPR)          
+         CLOSE (IPS)
+C        CLOSE (IPT)
+         CALL CPU_TIME(INSTANTE_FINAL)
+         WRITE (*,*)'TEMPO DE PROCESSAMENTO: ',
+     $      INSTANTE_FINAL-INSTANTE_INICIAL,' SEGUNDOS'
+         PAUSE
     
-		STOP
-      	END
+         STOP
+      END
 *
 *#######################################################################
 *#######################################################################
--- rcarrion/Nonsingd.for
+++ rcarrion/Nonsingd.for
@@ -1,112 +1,113 @@
 ************************************************************************
 *                                                                      *
-*  SUBROTINA QUE CALCULA OS COEFICIENTES DE [H] E [G]. O PONTO DE		 *
-*																	 *
-*    COLOCAÇÃO ESTÁ FORA DO ELEMENTO, ASSIM NÃO HÁ SINGULARIDADE.		 *
+*  SUBROTINA QUE CALCULA OS COEFICIENTES DE [H] E [G]. O PONTO DE        *
+*                                                                    *
+*    COLOCAÇÃO ESTÁ FORA DO ELEMENTO, ASSIM NÃO HÁ SINGULARIDADE.        *
 *                                                                      *
 *         A INTEGRAÇÃO É FEITA USANDO QUADRATURA GAUSSIANA             *
 *                                                                      *
 ************************************************************************
 *
       SUBROUTINE NONSINGD(ZHELEM,ZGELEM,CO,CXP,CYP,CZP,ETA,ZGE,ZCS,ZCP,
-     $DELTA,PI,FR,NPG)
+     $    DELTA,PI,FR,NPG)
 *
-	IMPLICIT REAL*8 (A-H,O-Y)
-	IMPLICIT COMPLEX*16 (Z)
-	COMMON INP,INQ,IPR,IPS,IPT
-	DIMENSION DELTA(3,3)
-	DIMENSION ZHELEM(3,3),ZGELEM(3,3),ZU(3,3),ZT(3,3)
-	DIMENSION GI(NPG),OME(NPG)
-	DIMENSION CO(4,3),ETA(3),P(2,4),XJ(2,3),F(4)
+        IMPLICIT REAL*8 (A-H,O-Y)
+        IMPLICIT COMPLEX*16 (Z)
+        COMMON INP,INQ,IPR,IPS,IPT
+        DIMENSION DELTA(3,3)
+        DIMENSION ZHELEM(3,3),ZGELEM(3,3),ZU(3,3),ZT(3,3)
+        DIMENSION GI(NPG),OME(NPG)
+        DIMENSION CO(4,3),ETA(3),P(2,4),XJ(2,3),F(4)
 *
 * ZERA AS MATRIZES ELEMENTARES HELEM E GELEM
 *
-	DO 20 JN=1,3
-	DO 20 IN=1,3
-	ZHELEM(IN,JN)=0.D0
-	ZGELEM(IN,JN)=0.D0
-  20	CONTINUE
+        DO 20 JN=1,3
+            DO 20 IN=1,3
+                ZHELEM(IN,JN)=0.D0
+                ZGELEM(IN,JN)=0.D0
+  20    CONTINUE
 *
 * ACIONA ROTINA QUE CALCULA OS PONTOS E PESOS DE GAUSS
 *
-	CALL GAULEG(-1.D0,1.D0,GI,OME,NPG)
+        CALL GAULEG(-1.D0,1.D0,GI,OME,NPG)
 *
-	DO 400 JG=1,NPG
+        DO 400 JG=1,NPG
 *
-      G2=GI(JG)
-      P2=OME(JG)
-      SP=1.D0+G2
-      SM=1.D0-G2
-      P(1,1)=-0.25D0*SM
-      P(1,2)= 0.25D0*SM
-      P(1,3)= 0.25D0*SP
-      P(1,4)=-0.25D0*SP
+            G2=GI(JG)
+            P2=OME(JG)
+            SP=1.D0+G2
+            SM=1.D0-G2
+            P(1,1)=-0.25D0*SM
+            P(1,2)= 0.25D0*SM
+            P(1,3)= 0.25D0*SP
+            P(1,4)=-0.25D0*SP
 *
-      DO 300 IG=1,NPG
+            DO 300 IG=1,NPG
 *
-      G1=GI(IG)
-      P1=OME(IG)
-      RP=1.D0+G1
-      RM=1.D0-G1
-      F(1)=0.25D0*RM*SM
-      F(2)=0.25D0*RP*SM
-      F(3)=0.25D0*RP*SP
-      F(4)=0.25D0*RM*SP
-      P(2,1)=-0.25D0*RM
-      P(2,2)=-0.25D0*RP
-      P(2,3)= 0.25D0*RP
-      P(2,4)= 0.25D0*RM
+                G1=GI(IG)
+                P1=OME(IG)
+                RP=1.D0+G1
+                RM=1.D0-G1
+                F(1)=0.25D0*RM*SM
+                F(2)=0.25D0*RP*SM
+                F(3)=0.25D0*RP*SP
+                F(4)=0.25D0*RM*SP
+                P(2,1)=-0.25D0*RM
+                P(2,2)=-0.25D0*RP
+                P(2,3)= 0.25D0*RP
+                P(2,4)= 0.25D0*RM
 *
 * CALCULA A RELAÇÃO ENTRE AS COORDENADAS CARTESIANAS E HOMOGÊNEAS
 *
-      DO 90 I=1,2
-      DO 80 J=1,3
-      TEMP=0.D0
-      DO 70 K=1,4
-      TEMP=TEMP+P(I,K)*CO(K,J)
-  70  CONTINUE
-      XJ(I,J)=TEMP
-  80  CONTINUE
-  90  CONTINUE
+                DO 90 I=1,2
+                    DO 80 J=1,3
+                        TEMP=0.D0
+                        DO 70 K=1,4
+                            TEMP=TEMP+P(I,K)*CO(K,J)
+  70                    CONTINUE
+                        XJ(I,J)=TEMP
+  80                CONTINUE
+  90            CONTINUE
 *
 * CALCULA O JACOBIANO
 *
-      DET=DSQRT((XJ(1,2)*XJ(2,3)-XJ(2,2)*XJ(1,3))**2 +
-     $          (XJ(2,1)*XJ(1,3)-XJ(1,1)*XJ(2,3))**2 +
-     $          (XJ(1,1)*XJ(2,2)-XJ(2,1)*XJ(1,2))**2  )
-      IF(DET-1.0D-5) 100,110,110
- 100  WRITE(IPR,1000) DET
-      WRITE(IPR,1100) ((CO(I,J),J=1,3),I=1,4)
- 1000 FORMAT(///' NONSING : ERRO, JACOBIANO NULO OU NEGATIVO =',D14.5)
- 1100 FORMAT(/1X,'COORDINADAS DOS PONTOS EXTREMOS '/(3D14.5))
-      STOP ' NONSING : ERRO, JACOBIANO NULO OU NEGATIVO'
- 110  CONTINUE
+                DET=DSQRT((XJ(1,2)*XJ(2,3)-XJ(2,2)*XJ(1,3))**2 +
+     $              (XJ(2,1)*XJ(1,3)-XJ(1,1)*XJ(2,3))**2 +
+     $              (XJ(1,1)*XJ(2,2)-XJ(2,1)*XJ(1,2))**2  )
+                IF(DET-1.0D-5) 100,110,110
+ 100            WRITE(IPR,1000) DET
+                WRITE(IPR,1100) ((CO(I,J),J=1,3),I=1,4)
+ 1000           FORMAT(///" NONSING : ERRO, JACOBIANO NULO OU NEGATIVO= 
+     $"             ,D14.5)
+ 1100           FORMAT(/1X,'COORDINADAS DOS PONTOS EXTREMOS '/(3D14.5))
+                STOP ' NONSING : ERRO, JACOBIANO NULO OU NEGATIVO'
+ 110            CONTINUE
 *
 * CALCULA AS COORDENADAS DO PONTO DE INTEGRAÇÃO
 *
-      CXG=0.D0
-      CYG=0.D0
-      CZG=0.D0
-      DO 130 I=1,4
-      CXG=CXG+CO(I,1)*F(I)
-      CYG=CYG+CO(I,2)*F(I)
-      CZG=CZG+CO(I,3)*F(I)
- 130  CONTINUE
+                CXG=0.D0
+                CYG=0.D0
+                CZG=0.D0
+                DO 130 I=1,4
+                    CXG=CXG+CO(I,1)*F(I)
+                    CYG=CYG+CO(I,2)*F(I)
+                    CZG=CZG+CO(I,3)*F(I)
+ 130            CONTINUE
 *
 * ACIONA ROTINA QUE CALCULA A SOLUÇÃO FUNDAMENTAL ESTÁTICA 3D
 *
-      CALL SOLFUND(ZU,ZT,CXP,CYP,CZP,CXG,CYG,CZG,ETA,ZGE,ZCS,ZCP,
-     $DELTA,PI,FR)
+                CALL SOLFUND(ZU,ZT,CXP,CYP,CZP,CXG,CYG,CZG,ETA,ZGE,ZCS,
+     $              ZCP,DELTA,PI,FR)
 *
-      P12=P1*P2*DET
-      DO 200 JN=1,3
-      DO 200 IN=1,3
-	ZHELEM(IN,JN)=ZHELEM(IN,JN)+ZT(IN,JN)*P12
-      ZGELEM(IN,JN)=ZGELEM(IN,JN)+ZU(IN,JN)*P12
- 200  CONTINUE
+                P12=P1*P2*DET
+                DO 200 JN=1,3
+                    DO 200 IN=1,3
+                        ZHELEM(IN,JN)=ZHELEM(IN,JN)+ZT(IN,JN)*P12
+                        ZGELEM(IN,JN)=ZGELEM(IN,JN)+ZU(IN,JN)*P12
+ 200            CONTINUE
 *
- 300  CONTINUE
- 400  CONTINUE
+ 300        CONTINUE
+ 400    CONTINUE
 *
-      RETURN
+        RETURN
       END
