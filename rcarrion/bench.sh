7a6379e79282a004efa081845b7da77478e071cf
Giuliano Augusto Faulin Belinassi <belinass@ime.usp.br>
2017-11-01 14:01:37 -0200

    Brenchmark scrit

--- rcarrion/bench.sh
+++ rcarrion/bench.sh
@@ -1,59 +1,49 @@
 #!/bin/bash
 
-make clean
-make gpu EXTRA="-DTEST_CUDA" -j 4
+STA_FILES=(ESOLO240E_-5+5.DAT ESOLO960E_-5+5_e10.DAT ESOLO2160E_-5+5.DAT ESOLO4000E_-20+20.DAT ESOLO14400E_-40+40.DAT)
+DYN_FILES=(ESOLO240D_-5+5.DAT ESOLO960D_-5+5_e10.DAT ESOLO2160D_-5+5.DAT ESOLO4000D_-20+20.DAT ESOLO14400D_-40+40.DAT)
+SOLE_FILES=(SSOLO240E_-5+5.DAT SSOLO960E_-5+5_e10.DAT SSOLO2160E_-5+5.DAT SSOLO4000E_-20+20.DAT SSOLO14400E_-40+40.DAT)
+SOLD_FILES=(SSOLO240D_-5+5.DAT SSOLO960D_-5+5_e10.DAT SSOLO2160D_-5+5.DAT SSOLO4000D_-20+20.DAT SSOLO14400D_-40+40.DAT)
+MESH_SIZE=(240 960 2160 4000 14400)
+COMPILER_PARAMS=(gpu gpu \ )
+MODE_STR=(gpu gpu_sing cpu)
+EXTRA_FLAGS=(\  -f \ )
 
-#warmup step
-for i in {1..3}; do
-./main
+NUM_WARMUPS=1
+NUM_EXECUTIONS=1
+
+for ((i=0; i<3; i++)); do
+	compiler_param=${COMPILER_PARAMS[$i]}
+	mode_str=${MODE_STR[$i]}
+	extra_flag=${EXTRA_FLAGS[$i]}
+
+	make clean
+	make ${compiler_param}
+
+	for ((j=0; j<3; ++j)); do
+		for thread in {24,48}; do
+			OMP_NUM_THREADS=${threads}
+
+			for ((k=0;k<5;k++)); do
+				mesh=${MESH_SIZE[$k]}
+				file_sta=${STA_FILES[$k]}
+				file_dyn=${DYN_FILES[$k]}
+				sol_sta=${SOLE_FILES[$k]}
+				sol_dyn=${SOLD_FILES[$k]}
+			
+				echo "Executando para $mesh em modo $mode_str com $thread threads"
+				echo ""
+				for ((l=1;l<=NUM_WARMUPS;l++)); do
+					echo "Warmup ${l}: "
+					./main $file_sta $file_dyn $sol_sta $sol_dyn $extra_flag
+				done
+				for ((l=1;l<=NUM_EXECUTIONS;l++)); do
+					echo "Execução número $l"
+					output_file="results/results_${mode_str}_${mesh}_${thread}_${l}.txt"
+					./main $file_sta $file_dyn $sol_sta $sol_dyn $extra_flag > ${output_file}
+				done
+			done
+		done
+	done
 done
-
-for i in {1..1}; do
-	echo "Executando em paralelo com 240: $i"
-	./main > "results/parallel_240_$i.txt"
-
-done
-
-for i in {1..1}; do
-	echo "Executando em paralelo com 960: $i"
-	./main ESOLO960E_-5+5_e10.DAT ESOLO960D_-5+5_e10.DAT SSOLO960E_-5+5_e10.DAT SSOLO960D_-5+5_e10.DAT > "results/parallel_960_$i.txt"
-done
-for i in {1..1}; do
-	echo "Executando em paralelo com 2160: $i"
-	./main ESOLO2160E_-5+5.DAT ESOLO2160D_-5+5.DAT SSOLO2160E_-5+5.DAT SSOLO2160D_-5+5.DAT > "results/parallel_2160_$i.txt"
-done
-for i in {1..1}; do
-	echo "Executando em paralelo com 4000: $i"
-	./main ESOLO4000E_-20+20.DAT ESOLO4000D_-20+20.DAT SSOLO4000E_-20+20.DAT SSOLO4000D_-20+20.DAT > "results/parallel_4000_$i.txt"
-done
-
-make clean
-make cpu
-
-OMP_NUM_THREADS=1
-#warmup
-for i in {1..1}; do
-	./main
-done
-
-for i in {1..1}; do
-	echo "Executando em série com 240: $i"
-	./main > "results/sequential_240_$i.txt"
-done
-
-for i in {1..1}; do
-	echo "Executando em série com 960: $i"
-	./main ESOLO960E_-5+5_e10.DAT ESOLO960D_-5+5_e10.DAT SSOLO960E_-5+5_e10.DAT SSOLO960D_-5+5_e10.DAT > "results/sequential_960_$i.txt"
-done
-
-for i in {1..1}; do
-	echo "Executando em série com 2160: $i"
-	./main ESOLO2160E_-5+5.DAT ESOLO2160D_-5+5.DAT SSOLO2160E_-5+5.DAT SSOLO2160D_-5+5.DAT > "results/sequential_2160_$i.txt"
-done
-
-for i in {1..1}; do
-	echo "Executando em série com 4000: $i"
-	./main ESOLO4000E_-20+20.DAT ESOLO4000D_-20+20.DAT SSOLO4000E_-20+20.DAT SSOLO4000D_-20+20.DAT > "results/sequential_4000_$i.txt"
-done
-
 
