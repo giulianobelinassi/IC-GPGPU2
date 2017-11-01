f6fb6debc6ad2488ce7629af48b588e6aaff89b0
Giuliano Augusto Faulin Belinassi <belinass@ime.usp.br>
2017-11-01 15:57:59 -0200

    Corrigido problema de usar mais threads que o especificado

--- rcarrion/bench.sh
+++ rcarrion/bench.sh
@@ -5,12 +5,12 @@ DYN_FILES=(ESOLO240D_-5+5.DAT ESOLO960D_-5+5_e10.DAT ESOLO2160D_-5+5.DAT ESOLO40
 SOLE_FILES=(SSOLO240E_-5+5.DAT SSOLO960E_-5+5_e10.DAT SSOLO2160E_-5+5.DAT SSOLO4000E_-20+20.DAT SSOLO14400E_-40+40.DAT)
 SOLD_FILES=(SSOLO240D_-5+5.DAT SSOLO960D_-5+5_e10.DAT SSOLO2160D_-5+5.DAT SSOLO4000D_-20+20.DAT SSOLO14400D_-40+40.DAT)
 MESH_SIZE=(240 960 2160 4000 14400)
-COMPILER_PARAMS=(gpu gpu \ )
+COMPILER_PARAMS=(\  gpu gpu \ )
 MODE_STR=(gpu gpu_sing cpu)
 EXTRA_FLAGS=(\  -f \ )
 
-NUM_WARMUPS=1
-NUM_EXECUTIONS=1
+NUM_WARMUPS=3
+NUM_EXECUTIONS=30
 
 for ((i=0; i<3; i++)); do
 	compiler_param=${COMPILER_PARAMS[$i]}
@@ -21,8 +21,8 @@ for ((i=0; i<3; i++)); do
 	make ${compiler_param}
 
 	for ((j=0; j<3; ++j)); do
-		for thread in {24,48}; do
-			OMP_NUM_THREADS=${threads}
+		for thread in {1,8,24,48}; do
+			export OMP_NUM_THREADS=${thread}
 
 			for ((k=0;k<5;k++)); do
 				mesh=${MESH_SIZE[$k]}
