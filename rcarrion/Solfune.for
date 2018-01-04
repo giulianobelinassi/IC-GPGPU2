7f29f1c3f9ce80dd17191df0bb02f70ba32de1e2
giulianobelinassi <giuliano.belinassi@usp.br>
2018-01-04 17:44:50 -0200

    Forgot to add Solfune.for

--- rcarrion/Solfune.for
+++ rcarrion/Solfune.for
@@ -4,13 +4,13 @@
 *																	 *
 ************************************************************************
 *
-      SUBROUTINE SOLFUNE(U,T,CXP,CYP,CZP,CXG,CYG,CZG,ETA,
-     $  N,NP,NPG,GE,RNU,RMU,C1,C2,C3,C4,DELTA)
+      SUBROUTINE SOLFUNE(T,CXP,CYP,CZP,CXG,CYG,CZG,ETA,
+     $  N,NP,NPG,RNU,RMU,C3,C4,DELTA)
 *
         IMPLICIT REAL (A-H,O-Y)
         IMPLICIT COMPLEX (Z)
         COMMON INP,INQ,IPR,IPS,IPT
-        DIMENSION U(3,3),T(3,3)
+        DIMENSION T(3,3)
         DIMENSION RD(3)
         DIMENSION ETA(3)
         DIMENSION DELTA(3,3)
@@ -26,7 +26,6 @@
 *
         DO 10 J=1,3
              DO 10 I=1,3
-                U(I,J)=(C1/R)*(C2*DELTA(I,J)+RD(I)*RD(J))
                 T(I,J)=(C3/(R*R))*(RDN*(C4*DELTA(I,J)+3.0*RD(I)*RD(J))+
      $              C4*(RD(J)*ETA(I)-RD(I)*ETA(J)))
   10    CONTINUE
