6543553b28f2382436a14db3613cc509482e142c
Giuliano Belinassi <giuliano.belinassi@usp.br>
2018-01-29 18:25:46 -0200

    better bench

--- rcarrion/bench.sh
+++ rcarrion/bench.sh
@@ -1,47 +1,44 @@
 #!/bin/bash
 
-STA_FILES=(ESOLO240E_-5+5.DAT ESOLO960E_-5+5_e10.DAT ESOLO2160E_-5+5.DAT ESOLO4000E_-20+20.DAT ESOLO14400E_-40+40.DAT)
-DYN_FILES=(ESOLO240D_-5+5.DAT ESOLO960D_-5+5_e10.DAT ESOLO2160D_-5+5.DAT ESOLO4000D_-20+20.DAT ESOLO14400D_-40+40.DAT)
-SOLE_FILES=(SSOLO240E_-5+5.DAT SSOLO960E_-5+5_e10.DAT SSOLO2160E_-5+5.DAT SSOLO4000E_-20+20.DAT SSOLO14400E_-40+40.DAT)
-SOLD_FILES=(SSOLO240D_-5+5.DAT SSOLO960D_-5+5_e10.DAT SSOLO2160D_-5+5.DAT SSOLO4000D_-20+20.DAT SSOLO14400D_-40+40.DAT)
-MESH_SIZE=(240 960 2160 4000 14400)
-COMPILER_PARAMS=(gpu gpu \ )
-MODE_STR=(gpu gpu_sing cpu)
-EXTRA_FLAGS=(\  -f \ )
+MESH_SIZE=(510 632 1600 6840)
+COMPILER_PARAMS=(\  gpu)
+MODE_STR=(cpu gpu)
 
-NUM_WARMUPS=3
-NUM_EXECUTIONS=30
+NUM_WARMUPS=1
+NUM_EXECUTIONS=3
 
-for ((i=0; i<3; i++)); do
+NUM_MODES=${#COMPILER_PARAMS[@]}
+NUM_MESHES=${#MESH_SIZE[@]}
+
+for ((i=0; i<${NUM_MODES}; i++)); do
 	compiler_param=${COMPILER_PARAMS[$i]}
 	mode_str=${MODE_STR[$i]}
 	extra_flag=${EXTRA_FLAGS[$i]}
 
 	make clean
-	make ${compiler_param}
+	FPREC=double make ${compiler_param}
 
-	for ((j=0; j<3; ++j)); do
-		for thread in {1,8,24,48}; do
-			export OMP_NUM_THREADS=${thread}
+	for thread in {4,4}; do
+		export OMP_NUM_THREADS=${thread}
 
-			for ((k=0;k<5;k++)); do
-				mesh=${MESH_SIZE[$k]}
-				file_sta=${STA_FILES[$k]}
-				file_dyn=${DYN_FILES[$k]}
-				sol_sta=${SOLE_FILES[$k]}
-				sol_dyn=${SOLD_FILES[$k]}
-			
-				echo "Executando para $mesh em modo $mode_str com $thread threads"
-				echo ""
-				for ((l=1;l<=NUM_WARMUPS;l++)); do
-					echo "Warmup ${l}: "
-					./main $file_sta $file_dyn $sol_sta $sol_dyn $extra_flag
-				done
-				for ((l=1;l<=NUM_EXECUTIONS;l++)); do
-					echo "Execução número $l"
-					output_file="results/results_${mode_str}_${mesh}_${thread}_${l}.txt"
-					./main $file_sta $file_dyn $sol_sta $sol_dyn $extra_flag > ${output_file}
-				done
+		for ((k=0;k<${MESH_SIZE};k++)); do
+			echo $k
+			mesh=${MESH_SIZE[$k]}
+			file_sta=E${mesh}e.dat
+			file_dyn=E${mesh}d.dat
+			sol_sta=S${mesh}e_${MODE_STR[$i]}_${thread}.dat 
+			sol_dyn=S${mesh}d_${MODE_STR[$i]}_${thread}.dat
+		
+			echo "Executando para $mesh em modo $mode_str com $thread threads"
+			echo ""
+			for ((l=1;l<=NUM_WARMUPS;l++)); do
+				echo "Warmup ${l}: "
+				./main $file_sta $file_dyn $sol_sta $sol_dyn
+			done
+			for ((l=1;l<=NUM_EXECUTIONS;l++)); do
+				echo "Execução número $l"
+				output_file="results/results_${mode_str}_${mesh}_${thread}_${l}.txt"
+				./main $file_sta $file_dyn $sol_sta $sol_dyn $extra_flag > ${output_file}
 			done
 		done
 	done
